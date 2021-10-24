01317d81ed310b7f5a5d885ea2cee1e60a5bb6a7
Nikita Abakumov <nabakumov@mt.1tv.ru>
2021-10-24 22:06:26 +0300

    finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import {Excel} from '@/components/excel/Excel'
+import {Formula} from './components/Formula/Formula'
+import {Header} from './components/header/Header'
+import {Table} from './components/Table/Table'
+import {Toolbar} from './components/Toolbar/Toolbar'
 import './scss/index.scss'
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
